d3a0cd386aa4ec08fb425e06f950c7a5111f6edd
asiaromaria <designsbyarc@gmail.com>
2021-09-22 19:58:57 -0700

    user schema set up

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const  mongoose  =  require('mongoose');
+const Joi = require('joi');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+
+ const  userSchema  =  new  mongoose.Schema({ 
+    name:  { type: String, required: true, minlength: 5, maxlength: 50 },
+    email: { type: String, unique: true, required: true, minlength: 5, maxlength: 255 },
+    password: { type: String, required: true, minlength: 5, maxlength: 1024 },
+    isAdmin: { type: Boolean, default: false},
+    }); 
+
+    userSchema.methods.generateAuthToken = function () {
+        return jwt.sign({ _id: this._id, name: this.name }, config.get('jwtSecret'));
+       };
+    const User = mongoose.model('User', userSchema);
+
+    function validateUser(user) {
+        const schema = Joi.object({
+            name: Joi.string().min(5).max(50).required(),
+            email: Joi.string().min(5).max(255).required().email(),
+            password: Joi.string().min(5).max(1024).required(),
+        });
+        return schema.validate(user);
+    }
+
+exports.User = User;
+exports.validateUser = validateUser;
+exports.userSchema = userSchema;
